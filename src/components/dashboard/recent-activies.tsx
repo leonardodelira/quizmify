8a823ca733be4dbf116b348074a7a42ba3775f85
leonardodelira <leonardo.dlira@mercadolivre.com>
2024-04-18 10:32:41 -0300

    feat: recent activities

--- src/components/dashboard/recent-activies.tsx
+++ src/components/dashboard/recent-activies.tsx
@@ -1,28 +1,22 @@
-"use client";
-import { useRouter } from "next/navigation";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { prisma } from "@/lib/db";
+import { getAuthSession } from "@/lib/nextauth";
 
 type Props = {};
 
-const RecentActivities = ({}: Props) => {
-  const router = useRouter();
+const RecentActivities = async ({}: Props) => {
+  const session = await getAuthSession();
+  const games_count = await prisma.game.count({
+    where: {
+      userId: session!.user.id,
+    },
+  });
 
   return (
-    <Card
-      className="col-span-4 lg:col-span-3"
-      onClick={() => router.push("/history")}
-    >
+    <Card className="col-span-4 lg:col-span-3">
       <CardHeader>
         <CardTitle className="text-2xl font-bold">Recent Activities</CardTitle>
-        <CardDescription>
-          You have played 5 quizzes in the last 7 days.
-        </CardDescription>
+        <CardDescription>You have played {games_count} quizzes in the last 7 days.</CardDescription>
       </CardHeader>
 
       <CardContent className="pl-2"></CardContent>
